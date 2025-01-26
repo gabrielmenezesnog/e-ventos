a4bf564fe772fe6ecf07a1e197fc450cff18fb6c
gabrielmenezesnog <gabriel.gmnogueira@gmail.com>
2025-01-26 14:21:59 -0300

    feat: adicionando botões de carrinho e compra nos cards da listagem

--- src/components/atoms/TicketsList/index.tsx
+++ src/components/atoms/TicketsList/index.tsx
@@ -2,6 +2,8 @@
 
 import React from "react";
 import { iTickets } from "@/interfaces/iTickets";
+import Link from "next/link";
+import Image from "next/image";
 
 interface iProps {
   tickets: iTickets[];
@@ -27,7 +29,7 @@ const TicketsList: React.FC<iProps> = ({ tickets, isLoading, vertical }) => {
       className={`mt-5 flex gap-5 pr-5 ${
         vertical
           ? "flex-col max-h-[800px] overflow-y-auto"
-          : "overflow-x-auto flex-row lg:max-w-[1320px] lg:overflow-x-scroll"
+          : "overflow-x-auto flex-row lg:max-w-[1320px] lg:overflow-x-scroll pb-5"
       } ml-5`}
     >
       {tickets.map((ticket) => (
@@ -38,17 +40,40 @@ const TicketsList: React.FC<iProps> = ({ tickets, isLoading, vertical }) => {
           }`}
         >
           <div className="p-4">
-            <h3 className="text-lg font-bold">{ticket.name}</h3>
+            <div className="flex flex-row items-center gap-2 mb-2">
+              <div className="h-2 w-3 bg-primary" />
+              <h3 className="text-lg font-bold">{ticket.name}</h3>
+            </div>
+
             <p className="text-sm text-gray-600 font-medium mb-5">
               R$ {ticket.price.toFixed(2)}
             </p>
+
             <p className="text-sm text-gray-600 mb-2">{ticket.local}</p>
+
             <p className="text-sm text-gray-600 mb-5">
               {new Date(ticket.date).toLocaleDateString("pt-BR")}
             </p>
+
             <p className="text-sm text-gray-600">
               {`${ticket.sold_quantity} ingressos vendidos 🔥`}
             </p>
+
+            <div className="flex flex-row items-center gap-2 mt-6">
+              <button className="bg-primary text-white font-semibold">
+                <Link href={`/event/${ticket.id}`}>Comprar</Link>
+              </button>
+
+              <button className="bg-gray_5 text-black font-semibold flex items-center justify-center gap-1">
+                <span>+</span>
+                <Image
+                  src="/images/shopping_cart.svg"
+                  alt="Adicionar ao Carrinho"
+                  width={20}
+                  height={20}
+                />
+              </button>
+            </div>
           </div>
         </li>
       ))}
